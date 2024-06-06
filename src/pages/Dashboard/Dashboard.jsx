4c3eddade755c47fa5b0db4479fa9f5df57bbde6
Mrsameershaikh <letsmailsameer@gmail.com>
2024-06-06 16:46:14 +0530

    templet added in dashboard page for nodes and node settings

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,25 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button } from '@chakra-ui/react'
+import React from 'react';
+import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
   return (
-    <Box bg='black' w='100%' h='100vh' p={4} color='white' overflow={'hidden'}>
-        dashboard
+    <Box bg='white' w='100%' h='100vh' overflow={'hidden'}>
+        <Box as='nav' w='100%' h={12} className={styles.navbarContainer}>
+          <Button float='right' h={8} border='1px solid gray'>Save Changes</Button>
+        </Box>
+        <Box w='100%' h='100%' className={styles.taskSection}>
+          
+          {/* left section which shows the nodes */}
+          <Box w='80%' h='100%' borderRight='1px solid gray' p={4}>
+              nodes will display here
+          </Box>
+
+          {/* right section which has node setting */}
+          <Box w='20%' h='100%' p={4}>
+            settings
+          </Box>
+        </Box>
     </Box>
   )
 }
